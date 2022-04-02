cac437971cd8eec09fad2c0be7ed255815a44b7b
Lucas ABM <lucasabmacedo@hotmail.com>
2022-04-02 02:27:36 -0300

    Implement Dijkstra algorithm

--- app/src/graphs/graph.py
+++ app/src/graphs/graph.py
@@ -1,7 +1,7 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 from pyvis import network as net
 import time
+from queue import PriorityQueue
 
 class Graph:
     def __init__(self, fd):
@@ -38,15 +38,17 @@ class Graph:
             self.graph.add_node(values[1], x = int(values[2])*50, y = int(values[3])*50)
             self.graph.add_node(values[4], x = int(values[5])*50, y = int(values[6])*50)
             
+            wheight_time = (int(values[7])/int(values[8]))*3600
+
             # Formata tempo em formato hh:mm
-            time_string = time.strftime('%H:%M', time.gmtime((int(values[7])/int(values[8]))*3600))
+            time_string = time.strftime('%H:%M', time.gmtime(wheight_time))
             edge_title = f"ID = {values[0]}<br>"
             edge_title += f"Speed = {values[8]}km/h<br>"
             edge_title += f"Distance = {values[7]}km<br>"
             edge_title += f"Time = {time_string}"
             
             #str(int(values[7])/int(values[8]))
-            self.graph.add_edge(values[1], values[4], distance=values[7], speed=values[8], title=edge_title)
+            self.graph.add_edge(values[1], values[4], distance=values[7], speed=values[8], time=wheight_time, title=edge_title)
 
     def showGraph(self):
         graph_plot = net.Network(height='100%', width='100%',notebook=False, directed=True)
@@ -55,6 +57,31 @@ class Graph:
         graph_plot.toggle_drag_nodes(False)
         graph_plot.show('graph.html')
 
-fid  = open("input.txt", "r")
+    def dijkstra(self, origin, destiny = None):
+        visited = {}
+        distances = {}
+        pq = PriorityQueue()
+      
+        for node in self.graph.nodes:
+            visited[node] = False
+            distances[node] = float('inf')
+
+        distances[origin] = 0
+
+        pq.put((distances[origin], origin))
+        while not pq.empty():
+            _, current = pq.get()
+            if not visited[current]:
+                visited[current] = True
+                for neighbor in list(self.graph.neighbors(current)):
+                    cost = self.graph.edges[current, neighbor]["time"]
+                    if distances[current] + cost <= distances[neighbor]:
+                        distances[neighbor] = distances[current] + cost
+                        pq.put((distances[neighbor], neighbor))
+        
+        if destiny is None: return distances
+        return distances[destiny]
+
+fid  = open("input2.txt", "r")
 g = Graph(fid)
 g.showGraph()
