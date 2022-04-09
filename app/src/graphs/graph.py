35be7abdca5cac0dfe39944792d74f85e64b1bbb
DYosplay <joaopedrofelix2013@hotmail.com>
2022-04-09 00:46:55 -0300

    yen algorithm for finding the k shortest paths

--- app/src/graphs/graph.py
+++ app/src/graphs/graph.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 import networkx as nx
 from pyvis import network as net
 import time
@@ -61,20 +62,20 @@ class Graph:
         Entrada:
             self: próprio grafo
             origin: origem da busca, é um inteiro como string. Ex: '2', '3'
-            destiny: destino da busca, segue o mesmo padrão de origin
+            destination: destino da busca, segue o mesmo padrão de origin
 
         Saída:
             Tupla: (caminho, distancia)
                 caminho: menor caminho encontrado entre a origem e destino
                 distancia: a distância deste menor caminho encontrado
     """
-    def getShortestPath(self, origin, destiny):
-        distance, prev = self.dijkstra(origin, destiny)
+    def getShortestPath(self, origin, destination):
+        distance, prev = self.dijkstra(origin, destination)
         path = []
 
-        if distance < float('inf'):
-            path.insert(0, destiny)
-            current = prev[destiny]
+        if distance[destination] < float('inf'):
+            path.insert(0, destination)
+            current = prev[destination]
             
             while current != origin:
                 path.insert(0, current)
@@ -88,7 +89,7 @@ class Graph:
         Entrada:
             self: próprio grafo
             origin: origem da busca, é um inteiro como string. Ex: '2', '3'
-            destiny: destino da busca, segue o mesmo padrão de origin
+            destination: destino da busca, segue o mesmo padrão de origin
 
         Saída:
             Tupla: (distancia, anteriores)
@@ -96,7 +97,7 @@ class Graph:
                 anteriores: dicionário em que os valores são associados às chaves que eles desembocam
                     Ex: '2': '1' significa que o vértice 2 é acessível através do vértice 1 (1 -> 2)
     """
-    def dijkstra(self, origin, destiny = None):
+    def dijkstra(self, origin, destination = None):
         visited = {}
         distances = {}
         pq = PriorityQueue()
@@ -121,13 +122,112 @@ class Graph:
                         
                         prev[neighbor] = current
         
-        if destiny is None: return distances
-        return distances[destiny], prev  
+        if destination is None: return distances
+        return distances, prev  
 
-fid  = open("input.txt", "r")
+    """
+        Entrada:
+            self: próprio grafo
+            origin: origem da busca, é um inteiro como string. Ex: '2', '3'
+            destination: destino da busca, segue o mesmo padrão de origin
+
+        Saída:
+            Tupla: (distancia, anteriores)
+                distancia: a distância deste menor caminho encontrado
+                anteriores: dicionário em que os valores são associados às chaves que eles desembocam
+                    Ex: '2': '1' significa que o vértice 2 é acessível através do vértice 1 (1 -> 2)
+    """
+    def yenkShortestPaths(self, origin, destination, k=5):
+        
+        # obtém o menor caminho da origem ao destino, além das listas das distâncias a cada vértice
+        path, distance = self.getShortestPath(origin, destination)
+
+        # A = k menores caminhos
+        A = [{'cost': distance[destination], 'path': path}]
+        
+        # B = canditados aos k menores caminhos
+        B = []
+
+        # garante que de fato existe um caminho. Caso não exista um caminho, retorna vazio
+        if not A[0]['path']:
+            return A
+
+        # Busca os k melhores caminhos
+        for i in range (1, k):
+
+            # esse loop acontece uma quantidade de vezes igual ao número de vértices do último melhor caminho - 2 (é como se ignorasse o vértice inicial e final)
+            for j in range (0, len(A[-1]['path']) - 1):
+                
+                # seleciona um vértice para refinamento
+                spur_node = A[-1]['path'][j]
+
+                # caminho do vértice inicial até o vértice de refinamento (incluso) (para análise)
+                path_root = A[-1]['path'][:j+1]
+
+                removed_edges = []
+
+                # para cada caminho em A
+                for path_k in A:
+                    current_path = path_k['path']
+                    
+                    # se o tamanho do caminho atual for maior que j
+                    # e o caminho para análise for igual ao caminho atual (até o vértice de refinamento -- incluso)
+                    if len(current_path) > j and path_root == current_path[:j+1]:
+                        
+                        # backup do peso da aresta
+                        weight = g.graph.edges[current_path[j], current_path[j+1]]['time']
+                        
+                        # caso o peso seja infinito apenas pulamos a iteração
+                        if weight == float('inf'):
+                            continue
+                        
+                        # muda temporariamente o peso (tempo de travessia) da aresta para infinito
+                        removed_edges.append((current_path[j], current_path[j+1], weight))
+                        g.graph.edges[current_path[j], current_path[j+1]]['time'] = float('inf')
+
+                # obtém um novo caminho e vetor de distâncias, dessa vez indo do nó de refinamento até o destino 
+                # (Busca alternativas desse nó de refinamento até o destino)
+                path_spur, distance_spur = self.getShortestPath(spur_node, destination)
+
+                # Se encontrou um novo caminho
+                if path_spur:
+                    # o caminho total passa a ser o caminho original concatenado com o novo caminho encontrado
+                    path_total = path_root[:-1] + path_spur
+
+                    # atualiza o vetor de distâncias de acordo com o novo caminho
+                    dist_total = distance[spur_node] + distance_spur[destination]
+
+                    # adiciona este caminho a lista de caminhos em potencial
+                    potential_path = {'cost': dist_total, 'path':path_total}
+                    if potential_path not in B:
+                        B.append(potential_path)
+
+                # volta os pesos para as arestas 
+                for edge in removed_edges:
+                    g.graph.edges[edge[0], edge[1]]['time'] = edge[2]
+            
+            # Se a lista com os candidatos a menores caminhos possuir elementos
+            if len(B):
+                # ordenamos estes caminhos de acordo com o custo
+                B = sorted(B, key=itemgetter('cost'))
+                # adicionamos o melhor destes caminhos a lista com os k menores caminhos
+                A.append(B[0])
+                # e o removemos da lista de candidatos
+                B.pop(0)
+            else:
+                break
+        
+        # A = k melhores caminhos
+        return A            
+
+
+
+                    
+
+fid  = open("input3.txt", "r")
 g = Graph(fid)
-g.graph.edges['2', '3']['distance'] = float('inf')
-print(g.graph.edges['2', '3']['distance'])
+#g.graph.edges['2', '3']['distance'] = float('inf')
+#print(g.graph.edges['2', '3']['distance'])
 #print(g.dijkstra('1', '4'))
-print(g.getShortestPath('1', '4'))
+print(g.yenkShortestPaths('1', '13'))
 g.showGraph()
